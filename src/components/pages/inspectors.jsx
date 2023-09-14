8a7a2bbe8b8328684155d12ca694c90e04a09dd6
alvarisolutions <paul.clark@alvari.solutions>
2023-09-15 02:46:24 +0800

    Tile fix

--- src/components/pages/inspectors.jsx
+++ src/components/pages/inspectors.jsx
@@ -506,14 +506,14 @@ class Inspectors extends React.Component {
     GenerateOpacity = (Name, Phone, Website, Qualification, PIInsurance ) => {
         const fullWebsite = 'http://' + Website
         return(
-            <div style={{backgroundColor:'rgba(0,0,0,0.6)', borderRadius:10, margin:10, width:'98%', height:100}}>
+            <div style={{backgroundColor:'rgba(0,0,0,0.6)', borderRadius:10, margin:10, width:'98%'}}>
                 
                 <Helmet>
                     <title>InspectWA</title>
                     <meta name="description" content="The Industry Association of Building and Property Inspectors in WA – Inspect WA. Inspect WA’s vision is to be Western Australia’s peak body representing professional independent building and property inspection businesses." />
                 </Helmet>
                 <div style={{display:'flex', flexDirection: 'row', width: '100%', textAlign:'left', padding:5}}>
-                    <div style={{width:'30%', height: 80, padding:10, contentAlign:'center', justifyContent:'center'}}>
+                    <div style={{width:'30%', padding:10, contentAlign:'center', justifyContent:'center'}}>
                         <span style={{color:'rgb(150,150,150)', fontSize:10}}>
                             INSPECTOR
                         </span>
@@ -523,7 +523,7 @@ class Inspectors extends React.Component {
                         </span>
                     
                     </div>
-                    <div style={{width:'30%', height: 80, contentAlign:'center', justifyContent:'center'}}>
+                    <div style={{width:'30%', contentAlign:'center', justifyContent:'center'}}>
                         <span style={{color:'rgb(150,150,150)', fontSize:10}}>
                             PHONE
                         </span>
@@ -543,7 +543,7 @@ class Inspectors extends React.Component {
                         </a>
                     
                     </div>
-                    <div style={{width:'40%', height: 80, contentAlign:'center', justifyContent:'center'}}>
+                    <div style={{width:'40%', contentAlign:'center', justifyContent:'center'}}>
                         <span style={{color:'rgb(150,150,150)', fontSize:10}}>
                             QUALIFICATION
                         </span>
